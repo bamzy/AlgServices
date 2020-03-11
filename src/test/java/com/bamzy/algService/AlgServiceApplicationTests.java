5dfc279276580d71c45c956f8b2848554fff675b
Bamdad <aghilide@ualberta.ca>
2020-03-11 07:56:54 -0400

    First commit

---
+++ src/test/java/com/bamzy/algService/AlgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamzy.algService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
